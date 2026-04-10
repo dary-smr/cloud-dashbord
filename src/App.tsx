e795769d3ea393df27ff9c0705ea3b0c9eab2f7a
Дарья <dashkin96@gmail.com>
2026-04-10 11:35:52 +0300

    подключен  API

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ResourceCard } from "./components/ResourceCard";
 
 type Status = "running" | "stopped" | "pending";
 
-function App() {
-  const [filter, setFilter] = useState<Status | "all">("all");;
+type Resource = {
+  id: number;
+  name: string;
+  status: Status;
+};
 
-  const resources: { id: number; name: string; status: Status }[] = [
-    { id: 1, name: "Cluster A", status: "running" },
-    { id: 2, name: "Cluster B", status: "stopped" },
-    { id: 3, name: "Cluster C", status: "pending" },
-  ];
+function App() {
+  const [resources, setResources] = useState<Resource[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState<Status | "all">("all");
 
   const filteredResources = resources.filter((r) => {
     if (filter === "all") return true;
     return r.status === filter;
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+  
+      // имитация запроса на сервер
+      const data: Resource[] = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve([
+            { id: 1, name: "Cluster A", status: "running" },
+            { id: 2, name: "Cluster B", status: "stopped" },
+            { id: 3, name: "Cluster C", status: "pending" },
+          ]);
+        }, 1000);
+      });
+  
+      setResources(data);
+      setLoading(false);
+    };
+  
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div  style={{ padding: "20px" }}>
