428e30c832a33a68bab4b18734d26cfb4e2dadfd
Дарья <dashkin96@gmail.com>
2026-04-13 19:56:47 +0300

    Добавлена обработка ошибок, показывается текст на экране, появляется кнопка Try again

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,49 @@ function App() {
   const [resources, setResources] = useState<Resource[]>([]);
   const [loading, setLoading] = useState(true);
   const [filter, setFilter] = useState<Status | "all">("all");
+  const [error, setError] = useState<string | null>(null);
 
   const filteredResources = resources.filter((r) => {
     if (filter === "all") return true;
     return r.status === filter;
   });
 
+  const fetchData = async () => {
+    try {
+    setError(null);
+    setLoading(true);
+
+    // запросы на сервер
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data: ApiUser[] = await response.json();
+    const mappedData: Resource[] = data.map(user => ({
+      id: user.id,
+      name: user.name,
+      status: ["running", "stopped", "pending"][
+        Math.floor(Math.random() * 3)
+      ] as Status,
+    }));
+
+    setResources(mappedData);
+  } catch (err) {
+    setError("Не удалось загрузить данные");
+  } finally {
+    setLoading(false);
+  }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-      setLoading(true);
-  
-      // запросы на сервер
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data: ApiUser[] = await response.json();
-      const mappedData: Resource[] = data.map(user => ({
-        id: user.id,
-        name: user.name,
-        status: ["running", "stopped", "pending"][
-          Math.floor(Math.random() * 3)
-        ] as Status,
-      }));
-  
-      setResources(mappedData);
-    } catch (error) {
-      console.error("Ошибка загрузки:", error);
-    } finally {
-      setLoading(false);
-    }
-    };
-  
     fetchData();
   }, []);
+
+  if (error) {
+    return (
+      <div>
+        <p>{error}</p>
+        <button onClick={fetchData}>Try again</button>
+      </div>
+    );
+  }
 
   if (loading) {
     return <div>Loading...</div>;
